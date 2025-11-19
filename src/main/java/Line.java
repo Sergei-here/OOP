00608a14503cbfc37d3ad73b6e92b4d62b3c32bc
Sergei Bychkov <sergei.by4kov.93@gmail.com>
2025-11-19 19:03:58 +0300

    ООП. Объекты и классы2. Линия

---
+++ src/main/java/Line.java
@@ -0,0 +1,26 @@
+public class Line {
+
+    Point start;
+    Point end;
+
+    public Line(int x1, int y1, int x2, int y2) {
+        this.start = new Point(x1, y1);
+        this.end = new Point(x2, y2);
+    }
+
+    public Line(Point end, Point start) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public String toString() {
+        return "Линия от " + end + " до " + start;
+    }
+
+
+    public double getLength() {
+        int deltaX = end.x - start.x;
+        int deltaY = end.y - start.y;
+        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+}
---
+++ src/main/java/Point.java
@@ -0,0 +1,15 @@
+public class Point { // создали класс Точка
+
+    int x, y;
+
+    public Point(int x, int y) { // объявили конструктор
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "{" + x + ";" + y + "}";
+    }
+}
+
+
